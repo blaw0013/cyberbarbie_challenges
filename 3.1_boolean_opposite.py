67d1a8e22dbc4344da933652f5a470ac1c22dd2d
Brian R. Lawler <78327678+blaw0013@users.noreply.github.com>
2021-02-07 08:21:36 -0500

    Add files via upload

---
+++ 3.1_boolean_opposite.py
@@ -0,0 +1,7 @@
+# Create a new list named opposite that contains the opposite boolean for each element in the list booleans.
+
+booleans = [False, False, True, True, False, True, False, True, False, False, False]
+opp_bool = [not x for x in booleans]
+print(opp_bool)
+
+# dang! I guessed this one and got it right!
