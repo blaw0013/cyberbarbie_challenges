eb62d0a815fbaa7fe00a5fde5cee8a17f92e3104
Brian R. Lawler <78327678+blaw0013@users.noreply.github.com>
2021-02-07 09:24:25 -0500

    Delete hello.py

--- hello.py
+++
@@ -1,5 +0,0 @@
-# Create a new list named greetings that adds "Hello, " in front of each name in the list names.
-
-names = ['Alice', 'Brian', 'Catherine','David','Elijah','Frances']
-hello = ['Hello ' + x for x in names]
-print(hello)
