d7078123e6e9f7a16de96654a4b594164a035696
Brian R. Lawler <78327678+blaw0013@users.noreply.github.com>
2021-02-07 08:09:27 -0500

    Add files via upload

---
+++ 2.3_name_length.py
@@ -0,0 +1,5 @@
+# Create a new list named lengths that contains the size of each name in the list of names
+
+names = ['Alice', 'Brian', 'Catherine','David','Elijah','Frances']
+name_length = [len(x) for x in names]
+print(name_length)
