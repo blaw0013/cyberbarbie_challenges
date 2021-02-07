d9fe141c8914ad028a1ad32a28fad4b8bda755a3
Brian R. Lawler <78327678+blaw0013@users.noreply.github.com>
2021-02-07 07:56:59 -0500

    Add files via upload

---
+++ 1.1_double_nums.py
@@ -0,0 +1,5 @@
+# Create a new list named double_nums by multiplying each number in nums by two.
+nums = [1, 2, 3, 4, 6, 9, 13, 17, 41]
+double_nums = [x*2 for x in nums]
+print(double_nums)
+
---
+++ 1.2_squares.py
@@ -0,0 +1,9 @@
+# Create a new list named squares that contains the square of every number in this list.
+nums = [1, 2, 3, 4, 6, 9, 13, 17, 41]
+squares = [x**2 for x in nums]
+print(squares)
+
+# Below I am just playing with cyberbarbies code
+numscb = range(11)  # note, begins at 0 and end when count reaches 11, thus the list is [0,1,2,...,10]
+squarescb = [x**2 for x in numscb]
+print(squarescb)
