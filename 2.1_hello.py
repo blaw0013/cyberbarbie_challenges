a9fae6274aff3ad8dce25d1df0a5f905beb5068c
Brian R. Lawler <78327678+blaw0013@users.noreply.github.com>
2021-02-07 08:06:53 -0500

    Add files via upload

--- 2.1_hello.py
+++ 2.1_hello.py
@@ -3,3 +3,7 @@
 names = ['Alice', 'Brian', 'Catherine','David','Elijah','Frances']
 hello = ['Hello ' + x for x in names]
 print(hello)
+
+# This prints the text Hello [name] but does not save to a new list
+for x in ['Alice', 'Brian', 'Catherine','David','Elijah','Frances']:
+    print('Hello ',x)
