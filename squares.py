7c81ad31b323d9ffc3b67912e5fa386a0735092c
Brian R. Lawler <78327678+blaw0013@users.noreply.github.com>
2021-02-06 22:19:22 -0500

    Add files via upload

--- squares.py
+++ squares.py
@@ -3,3 +3,7 @@ nums = [1, 2, 3, 4, 6, 9, 13, 17, 41]
 squares = [x**2 for x in nums]
 print(squares)
 
+# Below I am just playing with cyberbarbies code
+numscb = range(11)  # note, begins at 0 and end when count reaches 11, thus the list is [0,1,2,...,10]
+squarescb = [x**2 for x in numscb]
+print(squarescb)
