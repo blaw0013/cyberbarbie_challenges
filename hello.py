78c807d85a4bc29fc172aaf702f944d8a4406581
Brian R. Lawler <78327678+blaw0013@users.noreply.github.com>
2021-02-07 07:26:08 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,5 @@
+# Create a new list named greetings that adds "Hello, " in front of each name in the list names.
+
+names = ['Alice', 'Brian', 'Catherine','David','Elijah','Frances']
+hello = ['Hello ' + x for x in names]
+print(hello)
