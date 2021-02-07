ee1c46450fd2ea0154c4313ea9dc23b6f0e94426
Brian R. Lawler <78327678+blaw0013@users.noreply.github.com>
2021-02-07 09:23:14 -0500

    Add files via upload

--- 3.2_jerry.py
+++ 3.2_jerry.py
@@ -4,5 +4,8 @@
 
 names = ['Alice', 'Brian', 'Catherine','David','Elijah','Frances', 'Jerry']
 is_Jerry = ['True' if x == 'Jerry' else 'False' for x in names]
+# is_Jerry = [name == "Jerry" for name in names]
 print(is_Jerry)
 
+# cyberbarbie did more elegantly as follows; actually -- gives a boolean result instead of a text string
+# is_Jerry = [name == "Jerry" for name in names]
